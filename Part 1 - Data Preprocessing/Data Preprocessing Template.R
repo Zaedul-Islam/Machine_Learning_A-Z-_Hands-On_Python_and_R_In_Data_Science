4b16b5664213b1072d35a90c5c281ced89422b21
Zaedul Islam <islam.zaedul@gmail.com>
2020-03-09 00:17:15 +0600

    Import Data.csv dataset

--- Part 1 - Data Preprocessing/Data Preprocessing Template.R
+++ Part 1 - Data Preprocessing/Data Preprocessing Template.R
@@ -1,1 +1,5 @@
 # Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv("E:/WORKSTATION/Machine Learning Training/Machine Learning A-ZT Hands-On Python and R In Data Science/Part 1 - Data Preprocessing/Dataset/Data.csv") 
+
--- Part 1 - Data Preprocessing/Data Preprocessing Template.py
+++ Part 1 - Data Preprocessing/Data Preprocessing Template.py
@@ -4,3 +4,10 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('E:\WORKSTATION\Machine Learning Training\Machine Learning A-Z™ Hands-On Python and R In Data Science\Part 1 - Data Preprocessing\Dataset\Data.csv')
+# Creating a matrix of independent variables
+X = dataset.iloc[:, :-1].values
+# Creating dependent variable vector
+Y = dataset.iloc[:, 3].values
