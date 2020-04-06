ff1958758ba6522c5ad0775b179dbf059d6f2d74
Zaedul Islam <islam.zaedul@gmail.com>
2020-04-06 22:44:27 +0600

    Visualize the Test set results by plotting the regression line

--- Part 2 - Regression/Simple Linear Regression/Simple Linear Regression.py
+++ Part 2 - Regression/Simple Linear Regression/Simple Linear Regression.py
@@ -20,11 +20,23 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, rando
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, Y_train)
+regressor.fit(X_train, Y_train)￼
 
 # Predicting the Test set results
 Y_prediction = regressor.predict(X_test)
 
 # Visualizing the Training set results
 plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
+# Visualizing the Test set results
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
