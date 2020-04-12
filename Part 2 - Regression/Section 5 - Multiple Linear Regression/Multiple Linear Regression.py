0298ae681e2fbe5bfa98f796cdae7d2ac360a0c5
Zaedul Islam <islam.zaedul@gmail.com>
2020-04-12 21:26:12 +0600

    Read "50 Startups.csv" dataset

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Regression.py
@@ -0,0 +1,10 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+filepath = 'E:\\WORKSTATION\Machine Learning Training\\Machine Learning A-Z™ Hands-On Python and R In Data Science\\Part 2 - Regression\\Section 5 - Multiple Linear Regression\\Dataset\\50 Startups.csv'
+dataset = pd.read_csv(filepath)
