1453c2b6cad1acae29d2de82a1c8c7e7bf74d480
Zaedul Islam <islam.zaedul@gmail.com>
2020-04-12 21:49:15 +0600

    Split the dataset into the Training set and Test set

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Regression.py
@@ -8,3 +8,9 @@ import pandas as pd
 # Importing the dataset
 filepath = 'E:\\WORKSTATION\Machine Learning Training\\Machine Learning A-Z™ Hands-On Python and R In Data Science\\Part 2 - Regression\\Section 5 - Multiple Linear Regression\\Dataset\\50 Startups.csv'
 dataset = pd.read_csv(filepath)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, y_train, X_test, y_test = train_test_split(test_size = 0.2, random_state = 0)
