14533bed2c567cdfe09b3a893837a3a876195058
Zaedul Islam <islam.zaedul@gmail.com>
2020-04-12 22:01:44 +0600

    Encode the Independent Variable

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple Linear Regression.py
@@ -11,6 +11,14 @@ dataset = pd.read_csv(filepath)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
+# Encoding the categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:,3] = labelEncoder_X.fit_transform(X[:,3])
+oneHotEncoder = OneHotEncoder(categorical_features = [3])
+X = oneHotEncoder.fit_transform(X).toarray()
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, y_train, X_test, y_test = train_test_split(test_size = 0.2, random_state = 0)
+X_train, y_train, X_test, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
