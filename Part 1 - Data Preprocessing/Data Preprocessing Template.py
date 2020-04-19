8fdb82edd3338df2d68d8018263341d48a66eb1a
Zaedul Islam <islam.zaedul@gmail.com>
2020-04-19 23:32:14 +0600

    Fix a typing error

--- Part 1 - Data Preprocessing/Data Preprocessing Template.py
+++ Part 1 - Data Preprocessing/Data Preprocessing Template.py
@@ -36,8 +36,8 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 # Step 6: Feature Scaling
-from sklearn.preprocessing import StandardScalar
-sc_X = StandardScalar()
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 # For the test set we don’t need to fit the sc_X object because it's already fitted to the training set.
 X_test = sc_X.transform(X_test)
