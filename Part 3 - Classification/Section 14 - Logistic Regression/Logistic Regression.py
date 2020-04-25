ad81304efb793ff4290cd9e01bde0f639ccb4301
Zaedul Islam <islam.zaedul@gmail.com>
2020-04-26 00:30:31 +0600

    Complete data processing for Logistic Regression

---
+++ Part 3 - Classification/Section 14 - Logistic Regression/Logistic Regression.py
@@ -0,0 +1,25 @@
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+filepath = './Dataset/Social Network Ads.csv'
+dataset = pd.read_csv(filepath)
+# Creating a matrix of independent variables
+X = dataset.iloc[:,[2,3]].values
+# Creating dependent variable vector
+y = dataset.iloc[:,4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+X_train = standardScaler.fit_transform(X_train)
+X_test = standardScaler.transform(X_test)
+
