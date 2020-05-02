18ef23fe4a70559da27f0e7028ce16f428673ce8
Zaedul Islam <islam.zaedul@gmail.com>
2020-05-02 22:46:17 +0600

    Importing the dataset

---
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means Clustering.py
@@ -0,0 +1,10 @@
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+filepath = 'E:\\WORKSTATION\\Machine Learning Training\\Machine Learning A-Z™ Hands-On Python and R In Data Science\\Part 4 - Clustering\\Section 24 - K-Means Clustering\\Dataset\\Mall Customers.csv'
+dataset = pd.read_csv(filepath)
