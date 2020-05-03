5cacc5d2ca88662782088b7af1065757e48c352b
Zaedul Islam <islam.zaedul@gmail.com>
2020-05-03 15:18:25 +0600

    Visualizing the clusters

--- Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means Clustering.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means Clustering.py
@@ -38,3 +38,29 @@ kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10,
 # fit_predict method returns for each observation which cluster it belongs to. It will return
 # this cluster numbers into a single vector
 y_kmeans = kmeans.fit_predict(X)
+
+# Visualizing the clusters
+# Cluster are numbered from 0 to 4
+# X[y_kmeans == 0, 0] - Plotting the x coordinates of all the observation points that
+# belong to cluster 1
+# y_kmeans == 0 - Taking the cluster 1, 0 - Specifying the first
+# column of dataset X
+# X[y_kmeans == 0, 1] - Plotting the y coordinates of all the observation points that
+# belong to cluster 1
+# s - Size of a observation
+# c - Set the cluster color   
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+# Follow same thing for other 4 clusters
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 4')
+
+# Plot the centroids
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+
+plt.title('Clusters of clients')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score(1-100)')
+plt.legend()
+plt.show()
