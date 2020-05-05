fde8b20ed602baca02ac56bb95eb36a0f22e9868
Zaedul Islam <islam.zaedul@gmail.com>
2020-05-06 00:10:04 +0600

    Refactor comments

--- Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means Clustering.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means Clustering.py
@@ -15,7 +15,8 @@ from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11): # In range, the upper bound is exclusive
       # init - 'k-means++' intialization method overcomes the random initialization method trap
-      # max_iter - The maximum number of iterations there can be to find the final clusters when the K-Means algorithm is running. The default value for this parameter is 300.
+      # max_iter - The maximum number of iterations there can be to find the final clusters when the K-Means algorithm is running. 
+      # The default value for this parameter is 300.
       # n_init - The number of times the K-Mean algorithm will be run with different initial centroid. 
       # The default value for this parameter is 300.
       # random_state - This fixes all the random factors of the K-Means process. Setting it to zero will always give the same results
