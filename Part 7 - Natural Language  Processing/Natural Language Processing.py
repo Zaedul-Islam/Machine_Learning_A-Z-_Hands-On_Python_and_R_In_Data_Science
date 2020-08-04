14af6c0aa884c311f3437ce98880f7dcacdf1a89
Zaedul Islam <islam.zaedul@gmail.com>
2020-08-04 23:36:52 +0600

    Natural Language Processing -  New corpus of cleaned reviews

--- Part 7 - Natural Language  Processing/Natural Language Processing.py
+++ Part 7 - Natural Language  Processing/Natural Language Processing.py
@@ -32,29 +32,35 @@ from nltk.corpus import stopwords
 # Importing "PorterStemmer" class
 from nltk.stem.porter import PorterStemmer
 
-# 1.
-# 1st parameter - Only keeping the letters from a to z whether it's capital or not
-# 2nd parameter - Adding an space in between after removing non-significant characters
-review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
+# "corpus" will contain 1000 clean reviews
+# Initializing as an empty list
+corpus = [] 
 
-# 2. Convert all the uppercase letters to lowercase
-review = review.lower()
-
-# 3. Remove non-significant/irrelevant words (i.e., articles, prepositions)
-
-# Split the "review" string into individual words
-review = review.split()
-
-# 4. Stemming - Only keeping the root of the different words to simplify the review even more. If we keep all the different versions of the same word that 
-# means the same thing. Well, the problem is that in the end in our process in our sparse matrix we'll have tons of words and therefore huge sparsity and 
-# therefore our algorithm will have trouble to run as we'll simply have too many columns. In the sparse matrix each word will have its own column. 
-# Example: "loved", "loving" come from the same root word "love"
-# Creating an object of "PorterStemmer" class
-porterStemmer = PorterStemmer()
-
-# 5. Since "stopwords" is a package containing different lists of words in different languages. Here's the reviews are in English we need to make it easier 
-# for our program by telling it to only look at the English words and to specify this we need to explicitly mention it
-review = [porterStemmer.stem(word) for word in review if not word in set(stopwords.words('english'))]
-
-# 6. Joining back different words of this review list separated by a space
-review = ' '.join(review)
+for index in range(0, 1000):
+      # 1.
+      # 1st parameter - Only keeping the letters from a to z whether it's capital or not
+      # 2nd parameter - Adding an space in between after removing non-significant characters
+      review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][index])
+      
+      # 2. Convert all the uppercase letters to lowercase
+      review = review.lower()
+      
+      # 3. Remove non-significant/irrelevant words (i.e., articles, prepositions)
+      
+      # Split the "review" string into individual words
+      review = review.split()
+      
+      # 4. Stemming - Only keeping the root of the different words to simplify the review even more. If we keep all the different versions of the same word that 
+      # means the same thing. Well, the problem is that in the end in our process in our sparse matrix we'll have tons of words and therefore huge sparsity and 
+      # therefore our algorithm will have trouble to run as we'll simply have too many columns. In the sparse matrix each word will have its own column. 
+      # Example: "loved", "loving" come from the same root word "love"
+      # Creating an object of "PorterStemmer" class
+      porterStemmer = PorterStemmer()
+      
+      # 5. Since "stopwords" is a package containing different lists of words in different languages. Here's the reviews are in English we need to make it easier 
+      # for our program by telling it to only look at the English words and to specify this we need to explicitly mention it
+      review = [porterStemmer.stem(word) for word in review if not word in set(stopwords.words('english'))]
+      
+      # 6. Joining back different words of this review list separated by a space
+      review = ' '.join(review)
+      corpus.append(review)
